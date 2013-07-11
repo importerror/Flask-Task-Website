e023ef91f9456f9ae2948f73e8b264242f2b691f
san00719 <san00719@yahoo.com>
2013-07-12 00:51:50 +0530

    login Page created

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import *
 
 app=Flask(__name__)
  
@@ -9,6 +9,20 @@ def index():
 @app.route('/welcome')
 def welcome():
  	return render_template('welcome.html')
+
+@app.route('/hello')
+def hello():
+	return render_template('hello.html')
+
+@app.route('/Login',methods=['GET','POST'])
+def login():
+	error=None
+	if request.method=='POST':
+		if request.form['username']!='admin' or request.form['password']!='admin':
+			error="The Username or Password you entered is Invalid!"
+		else:
+			return redirect(url_for('hello'))
+	return render_template('login.html',error=error)
 
 if __name__=='__main__':
 	app.run(debug=True)
