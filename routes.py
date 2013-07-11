e0f486739183aa7b62d89446200ac9b225ea1a3c
san00719 <san00719@yahoo.com>
2013-07-11 19:39:43 +0530

    Static part of the site

---
+++ routes.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+ 
+@app.route('/')
+def index():
+	return render_template('home.html')
+ 
+@app.route('/welcome')
+def welcome():
+ 	return render_template('welcome.html')
+
+if __name__=='__main__':
+	app.run(debug=True)
