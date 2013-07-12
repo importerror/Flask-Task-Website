0fdb4877181630cb868e7b5ba497b001afc7d5f8
san00719 <san00719@yahoo.com>
2013-07-12 15:10:31 +0530

    Session enabled

--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
 from flask import *
+from functools import wraps
 
 app=Flask(__name__)
+ 
+app.secret_key='Working'
  
 @app.route('/')
 def index():
@@ -10,7 +13,24 @@ def index():
 def welcome():
  	return render_template('welcome.html')
 
+def login_required(func):
+	@wraps(func)
+	def wrap(*args,**kwargs):
+		if 'logged_in' in session:
+			return func(*args, **kwargs)
+		else:
+			flash('You need to login first')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in',None)
+	flash("You are now Logged out.")
+	return redirect(url_for('login'))
+
 @app.route('/hello')
+@login_required
 def hello():
 	return render_template('hello.html')
 
@@ -21,6 +41,7 @@ def login():
 		if request.form['username']!='admin' or request.form['password']!='admin':
 			error="The Username or Password you entered is Invalid!"
 		else:
+			session['logged_in']=True
 			return redirect(url_for('hello'))
 	return render_template('login.html',error=error)
 
